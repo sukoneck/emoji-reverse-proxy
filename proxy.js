4ea0072cc55a2cbd81f1cf73f9fcf6b171101ae5
sukoneck <19413126+sukoneck@users.noreply.github.com>
2021-08-31 20:33:24 -0700

    clean up for prod

--- proxy.js
+++ proxy.js
@@ -5,7 +5,9 @@ const http = require('http');
 const PORT = process.env.PORT || 8888
 const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/fdf2a581f7d945feaac1377227dd5c61"));
 const contract_address = '0xee4C821ed264916d1c035515703F8980410FC149';
+const default_dest = "https://nftnft.lol";
 
+// abi for the function that tells you what the redirect is
 function abiRedirects(names_result) { 
     return web3.eth.abi.encodeFunctionCall({
         "inputs": [
@@ -57,43 +59,40 @@ async function sendIt(hex) {
 http.createServer(async function (request, response) {
     let sub_puny = request.headers.host.split(".")[0];
     let sub_uni = punycode.toUnicode(sub_puny);
-    if (sub_uni == "www" || sub_uni.includes("localhost") || sub_uni.includes("nancy") || request.headers.host.includes("heroku")) {
-        
-        // TODO create a second heroku app using the same name? Do CNAMEs work for this instead?
 
-        let content = "Welcome to the homepage! \n\nYou either used www or you didn't include a subdomain at all"
-        console.log(content);
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write(content);
+    // if they tryna go home then just let 'em go home 
+    if (sub_uni == "www" || sub_uni.includes("localhost") || sub_uni.includes("nancy") || request.headers.host.includes("heroku")) {
+        console.log("received subdomain: " + sub_puny + ", so forwarding to default: " + default_dest);
+        response.writeHead(301, { "Location": dest });
         response.end();
     } else {
         try {
             let sub_hex = web3.utils.utf8ToHex(sub_uni);
             let w3_redirect = await sendIt(sub_hex); 
+            
+            // if there isn't a redirect registered, then send it to default
             if (w3_redirect == "") {
-                let content = "no redirect registered in web3 for: " + sub_puny + "\n" + w3_redirect;
-                console.log(content);
-                response.writeHead(200, { "Content-Type": "text/plain" });
-                response.write(content);
+                console.log("received subdomain: " + sub_puny + ", which doesn't have a redirect, so forwarding to default: " + default_dest);
+                response.writeHead(301, { "Location": dest });
                 response.end();
             } else {
 
-                // TODO filter for "http" formatting e.g.  web3.utils.toHex("🍔") = '0xf09f8d94' comes back without a colon?!?!
-
-                let content = "let's go: " + w3_redirect;
-                console.log(content);
+                // if their redirect already has http, then just send it fam
                 if (w3_redirect.includes("http")) {
-                    console.log(w3_redirect);
+                    console.log("resolved redirect to: " + w3_redirect);
                     response.writeHead(301, { "Location": w3_redirect });
-                    response.end();                    
+                    response.end();
+
+                // if redirect is chill and doesn't have http in it, then prepend http and send it fam
+                } else {
+                    let dest = "https://" + w3_redirect;
+                    console.log("resolved redirect to: " + dest);
+                    response.writeHead(301, { "Location": dest });
+                    response.end();
                 }
-                let dest = "http://" + w3_redirect;
-                console.log(dest);
-                response.writeHead(301, { "Location": dest });
-                response.end();
             }
         } catch (error) {
-            console.log(error);
+            failureCallback(error);
         }
     }
 }).listen(PORT);
